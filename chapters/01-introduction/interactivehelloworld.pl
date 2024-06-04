afd1ce5fbbd190d9c02e059603e0c2f0e810f2f8
Ive Never Finished <iveneverfinished@gmail.com>
2024-06-04 17:50:59 +1200

    Change the script to respect Bob
    Added a little control structure to change the output, based on the
    users input. For now, we only want to greet Bob. Why wouldn't we?
    Bob's fantastic.

--- chapters/01-introduction/interactivehelloworld.pl
+++ chapters/01-introduction/interactivehelloworld.pl
@@ -2,4 +2,8 @@
 print "What's your name? ";
 $name = <STDIN>;
 chomp ($name);
-print "'sup, $name?\n";
+if ($name eq "Bob") {
+	print "Bob! You've finally returned.\n";
+} else {
+	print "'sup, $name?\n";
+}
