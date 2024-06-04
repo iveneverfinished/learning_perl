d5326892fefce1af8ddc9e53f69edd612579878e
Ive Never Finished <iveneverfinished@gmail.com>
2024-06-04 18:02:37 +1200

    Add secret guessing
    One more interesting branch. If you're not Bob, you need to know the
    secret. Which is also bob. But lowercase. What a twist.

--- chapters/01-introduction/interactivehelloworld.pl
+++ chapters/01-introduction/interactivehelloworld.pl
@@ -2,8 +2,16 @@
 print "What's your name? ";
 $name = <STDIN>;
 chomp ($name);
+$secretword = "bob";
 if ($name eq "Bob") {
 	print "Bob! You've finally returned.\n";
 } else {
-	print "'sup, $name?\n";
+	print "'sup, $name?\nWhat's the codeword? ";
+	$guess = <STDIN>;		
+	chomp ($guess);
+	while ($guess ne $secretword) {
+		print("Nah, that's not right! Try again... ");
+		$guess = <STDIN>;
+		chomp ($guess);	
+	}
 }
